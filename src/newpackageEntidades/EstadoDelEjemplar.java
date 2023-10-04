2e142de220e4aaac4d6d6152a6543f22b3d89cfa
LoretoLola105 <141672893+LoretoLola105@users.noreply.github.com>
2023-10-04 17:37:18 -0300

    ENVIO CLASE ENUM
    ESTADO DEL EJEMPLAR

---
+++ src/newpackageEntidades/EstadoDelEjemplar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackageEntidades;
+
+/**
+ *
+ * @author Otras
+ */
+public enum EstadoDelEjemplar {
+  //contiene constantes SE COLOCAN EN MAYUSCULAS
+    PRESTADO,
+    RETRASO, 
+    REPARACION,
+    DISPONIBLE
+      
+}
